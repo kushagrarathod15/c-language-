b95129549325eb40c8a129f66eca22e5c45ad245
kushagrarathod15 <kushagrarathod6@gmail.com>
2025-09-24 19:16:54 +0530

    Create adress.c

---
+++ adress.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+ {
+    printf("kushagra singh rathod\n");
+    printf("50,krishna vihar\n");
+    printf("rampur,uttar pradesh\n");
+    printf("India - 244901\n");
+}
