eedd72296c42362c6a597b222f4cefcaca641040
kushagrarathod15 <kushagrarathod6@gmail.com>
2025-09-24 19:10:15 +0530

    Create operator.c

---
+++ operator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main() 
+{
+    double num1, num2, result;
+    char operator;
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    switch (operator) 
+    {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '/':
+            if (num2 != 0) 
+            {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            }
+             else 
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+    return 0;
+}
